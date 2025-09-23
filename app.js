5201b7744fb01c149d5de9854995cfe9549767ed
Jorge Reyes Rodríguez <jorgereyrodz@gmail.com>
2025-09-22 19:42:06 -0600

    Avance

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
-function agregarAmigo(nombre) {
-    let inputAmigo = document.getElementById("amigo");
-    let amigo = inputAmigo.ariaValue;
-
-    amigos.push(amigo)
+function agregarAmigo () {
+    const texto = document.getElementById('amigo');
+    const button container = document.getElementById('listadoAmigos')
+        
+    }
 
 }
